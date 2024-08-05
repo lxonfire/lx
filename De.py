e4462c560f901384e23af866c1cbee2d938dd0ab
lxonfire <149894699+lxonfire@users.noreply.github.com>
2024-08-05 09:24:20 +0600

    Update De.py

--- De.py
+++ De.py
@@ -9,9 +9,9 @@ MESSAGE = 'This is an hourly message from your bot!'
 def send_message(token, chat_id, message):
     url = f'https://api.telegram.org/bot{token}/sendMessage'
     payload = {
-        'chat_id': chat_id,
-        'text': message
-    }
+      'chat_id': chat_id,
+      'text': message
+    }
     response = requests.post(url, data=payload)
     return response.json()
 
